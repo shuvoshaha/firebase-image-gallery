d5e554d9a4a6346c35028e18bc5ad4d895fa4d03
shuvoshaha <shaha460@gmail.com>
2020-12-24 21:03:57 +0600

    congif firease api

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+ apiKey: "AIzaSyDVRWWrGN9zYOXcXPtFbvrSbMFPmG40Gbs",
+ authDomain: "image-gallery-ffe0b.firebaseapp.com",
+ projectId: "image-gallery-ffe0b",
+ storageBucket: "image-gallery-ffe0b.appspot.com",
+ messagingSenderId: "515772768826",
+ appId: "1:515772768826:web:e289eac2a563deb8480b03"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
